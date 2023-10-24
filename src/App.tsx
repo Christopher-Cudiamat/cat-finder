6df9e34831c496260d813bba731afda8e74baaa7
Christopher <christopher.cudiamat@jagaad.com>
2023-10-24 13:29:56 +0200

    feat: initial project setup with necessary libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import styled from 'styled-components';
 
-function App() {
+const App: React.FC = () => {
+
+  const StyledWrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <StyledWrapper>
+        <Button>INITIAL SET-UP</Button>
+      </StyledWrapper>
     </div>
   );
 }
