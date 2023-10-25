7e4010cb05f6e6cf935516524c3099a4f4fe9851
Christopher <christopher.cudiamat@jagaad.com>
2023-10-25 21:55:26 +0200

    revert: remove header component

--- src/components/Header/Header.styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import Navbar from 'react-bootstrap/Navbar';
-
-export const StyledHeader = styled(Navbar)`
-  background-image: linear-gradient(120deg, #fccb90 0%, #d57eeb 100%);
-  color: white;
-  padding: 2rem;
-`;
