b2fd1e959266a819739c8747903a61682f58919b
Christopher <christopher.cudiamat@jagaad.com>
2023-10-25 20:45:41 +0200

    feat: create cat page initial structure

--- src/pages/Cat/Cat.component.tsx
+++ src/pages/Cat/Cat.component.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import useFetch from 'hooks/useFetch';
+import { useNavigate, createSearchParams, useParams } from 'react-router-dom';
 
 const Cat: React.FC = () => {
-  return <div>Cat</div>;
+  const { id } = useParams();
+  console.log('id', id);
+  const navigate = useNavigate();
+  const {
+    data: { url, breeds },
+  } = useFetch(`https://api.thecatapi.com/v1/images/${id}`);
+  console.log('data', breeds);
+  const handleBack = () => {
+    navigate({
+      pathname: '/',
+      search: createSearchParams({
+        breed: breeds[0].id,
+      }).toString(),
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={handleBack}>BACK</button>
+      {breeds && (
+        <div>
+          <img
+            src={url}
+            alt=''
+          />
+          <p>{breeds[0].name}</p>
+          <p>{breeds[0].origin}</p>
+          <p>{breeds[0].description}</p>
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default Cat;
