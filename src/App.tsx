3086ebdbe5954d4acd9adfff16afd90148107fb9
Christopher <christopher.cudiamat@jagaad.com>
2023-10-25 14:23:14 +0200

    revert: global context code placement

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import theme from 'themeGlobal/themeGlobal';
-import { useGlobalContext } from 'hooks/useGlobalContext';
+import GlobalContextProvider from 'hooks/useGlobalContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ThemeProvider } from 'styled-components';
 import Home from 'pages/Home/Home.component';
 import Cat from 'pages/Cat/Cat.component';
-import Header from 'components/Header/Header.component';
 
 const App: React.FC = () => {
-  const {
-    globalState: { breedId },
-  } = useGlobalContext();
-
   return (
     <React.Fragment>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Header />
-          <Routes>
-            <Route
-              path='/'
-              element={<Home key={breedId} />}
-            />
-            <Route
-              path='/cat'
-              element={<Cat />}
-            />
-          </Routes>
-        </Router>
-      </ThemeProvider>
+      <GlobalContextProvider>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Routes>
+              <Route
+                path='/'
+                element={<Home />}
+              />
+              <Route
+                path='/cat'
+                element={<Cat />}
+              />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </GlobalContextProvider>
     </React.Fragment>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import GlobalContextProvider from 'hooks/useGlobalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalContextProvider>
-      <App />
-    </GlobalContextProvider>
+    <App />
   </React.StrictMode>
 );
 
