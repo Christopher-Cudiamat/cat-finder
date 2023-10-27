a0762388eec246eaf330de3646dcef90a7c36d64
Christopher <christopher.cudiamat@jagaad.com>
2023-10-27 22:45:54 +0200

    revert: remove loader component

--- src/components/Loader/Loader.component.tsx
+++ src/components/Loader/Loader.component.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { StyledLoader, StyledLoaderBackground } from './Loader.styled';
+import { useGlobalContext } from 'hooks/useGlobalContext';
 
 const Loader: React.FC = () => {
+  const {
+    globalState: { loading },
+  } = useGlobalContext();
+
   return (
     <React.Fragment>
-      <StyledLoaderBackground>
-        <StyledLoader
-          animation='border'
-          role='status'
-          variant='primary'
-        >
-          <span className='visually-hidden'>Loading...</span>
-        </StyledLoader>
-      </StyledLoaderBackground>
+      {loading ? (
+        <StyledLoaderBackground>
+          <StyledLoader
+            animation='border'
+            role='status'
+            variant='primary'
+          >
+            <span className='visually-hidden'>Loading...</span>
+          </StyledLoader>
+        </StyledLoaderBackground>
+      ) : null}
     </React.Fragment>
   );
 };
